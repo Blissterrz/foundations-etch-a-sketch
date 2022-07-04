1a18af945ec27217cf3fee23bcae214ea6233c2e
blissterrz <laithalnajjar2004@gmail.com>
2022-07-04 18:12:54 +0300

    Implement changing size of grid

--- script.js
+++ script.js
@@ -16,14 +16,9 @@ function createGrid(cellNum) {
        grid.appendChild(row);
     };
 }
-    
-createGrid(16);
 
-//Main hover functionality
-let cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => cell.addEventListener('mouseover', (e) => {
-    cell.setAttribute('style', 'background: black;');
-}));
+//Start the game
+createGrid(16);
 
 //Reset button
 function reset() {
@@ -32,3 +27,38 @@ function reset() {
 let resetBtn = document.querySelector('.reset');
 resetBtn.addEventListener('click', reset);
 
+//Setting the number of cells button
+function getSetSize() {
+    let setNum = prompt('Choose the number of squares per side for the grid.');
+    if (setNum > 100) {
+        alert('The number you have entered is too large, please enter a smaller number.');
+        return getSetSize();
+    } else if (setNum <= 0) {
+        alert('The number you have entered is too small, please enter a larger number');
+        return getSetSize();
+    } else if (isNaN(setNum)) {
+        alert('Please enter a number.');
+        return getSetSize();
+    } else {
+        return setNum;
+    }
+}
+function setSize() {
+    grid.textContent = '';
+    let setNum = getSetSize();
+    createGrid(setNum);
+    cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => cell.addEventListener('mouseover', (e) => {
+        cell.setAttribute('style', 'background: black;');
+    }));
+};
+let setBtn = document.querySelector('.setCells');
+setBtn.addEventListener('click', setSize);
+
+
+
+//Main hover functionality
+let cells = document.querySelectorAll('.cell');
+cells.forEach((cell) => cell.addEventListener('mouseover', (e) => {
+    cell.setAttribute('style', 'background: black;');
+}));
